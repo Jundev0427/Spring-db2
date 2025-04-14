b35817eceb6cc5cd710f3b1014481517bdf63024
Jundev0427 <jundev0427@gmail.com>
2025-04-14 15:08:08 +0900

    데이터 접근 기술 - 활용방안

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -15,7 +15,7 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 //@Import(MemoryConfig.class)
 @Slf4j
-@Import(QueryDslConfig.class)
+@Import(V2Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
